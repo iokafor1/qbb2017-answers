3807755c4a0e2383ed2ea4edd6d10ae89d1e64af
iokafor1 <iokafor1@jh.edu>
2017-08-31 08:42:20 -0400

    new python matcher code

--- day3-homework/kmer_matcher.py
+++ day3-homework/kmer_matcher.py
@@ -12,7 +12,6 @@ target = open(sys.argv[1])
 query = open(sys.argv[2])
 k = int(sys.argv[3])
 
-count = 0
 
 #create dictionary
 
@@ -23,19 +22,19 @@ for ident, sequence in fasta.FASTAReader( target ): #for identifier and sequence
     for i in range( 0, len(sequence) - k +1): #for each line from start to length of sequence
         kmer = sequence[i:i+k] #kmer is defined as sequence length to of next kmer
         if kmer not in kmer_dict: #if kmer is not dictionary add it
-            kmer_dict[kmer] = [i]
+            kmer_dict[kmer] = [(ident,i)] # key kmer and value position and name "tuple"
         else:
-            kmer_dict[kmer].append(i) #if kmer is in dictionary append position
-ident, sequence in fasta.FASTAReader( query ).next() #for identifier and sequence from fasta reader file "query"
-sequence = sequence.upper() #define sequence 
-for i in range( 0, len(sequence) - k +1): #for each line from start to length of sequence
-    kmer = sequence[i:i+k]
-    if count >= 1000:
-        break
-    else:
-        if kmer in kmer_dict:
-            print  kmer_dict[kmer],"\t",i,"\t",kmer 
-            count += 1
+            kmer_dict[kmer].append((ident,i)) #if kmer is in dictionary append position and ident
+ident, sequence_q = fasta.FASTAReader( query ).next() #for identifier and sequence from fasta reader file "query"
+sequence_q = sequence.upper() #define sequence 
+for j in range( 0, len(sequence_q) - k +1): #for each line from start to length of sequence
+    kmer_q = sequence_q[j:j+k]
+    if kmer_q in kmer_dict:
+        result = kmer_dict[kmer_q]
+        for item in result:
+            print item[0],"\t",item[1],"\t",j,"\t", kmer_q
+
+  
             
     
             
